581490fe53e748bb9c135b4eb09822da19d41043
Max Base <MaxBaseCode@gmail.com>
2020-06-01 17:44:09 +0430

    Step 4

--- tlg.js
+++ tlg.js
@@ -129,13 +129,13 @@ function get_mtprotoprocdata(e){
 					}
 				}else{
 					if(e.data[1].id == "PhoneCode"){
-						tg_out.innerHTML += "name: "+ob.first_name+"<br>"
-						tg_out.innerHTML += "last name: "+ob.last_name+"<br>"
-						tg_out.innerHTML += "user name: "+ob.username+"<br>"
-						tg_out.innerHTML += "phone: "+ob.phone+"<br>"
-						//tg_out.innerHTML += "photo id: "+ob.photo+"<br>"
-						tg_out.innerHTML += "status: "+ob.status+"<br>"
-
+						mtproto_state.innerHTML = "login Ok"
+						tg_out.innerHTML += "name: "+ob.user.first_name+"<br>"
+						tg_out.innerHTML += "last name: "+ob.user.last_name+"<br>"
+						tg_out.innerHTML += "user name: "+ob.user.username+"<br>"
+						tg_out.innerHTML += "phone: "+ob.user.phone+"<br>"
+						tg_out.innerHTML += "status: "+ob.user.status.was_online+"<br>"
+						//mode = 10 //connected to server and logined
 					}					
 				}
 				break
@@ -153,81 +153,105 @@ function get_mtprotoprocdata(e){
 }
 
 function parse_answer(body){
+	var isrec = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : false
+	var res = null
 	var ret = null
+	var len = 0
+	var _flags = 0
+	var request = {}
+	var value = 0
 	var tl_constructor = readUInt32LE(body,0)
 	body = body.slice(4) //remove tl_constructor
 	switch (tl_constructor){
 		case 0x8e1a1775:{ //nearestDc#8e1a1775 country:string this_dc:int nearest_dc:int = NearestDc;
-			ret = pick_out(body,{counrty:"string",this_dc:"uint4",nearest_dc:"uint4"})
+			request = {counrty:"string",this_dc:"uint4",nearest_dc:"uint4"}
 			break
 		}
 		case 0x2215bcbd:{//auth.sentCode#2215bcbd phone_registered:Bool phone_code_hash:string = auth.SentCode;
-			ret = pick_out(body,{phone_registered:"bool",phone_code_hash:"string"})
+			request = {phone_registered:"bool",phone_code_hash:"string"}
 			break
 		}
 		case 0x5e002502:{// flags:# type:auth.SentCodeType phone_code_hash:string next_type:flags.1?auth.CodeType timeout:flags.2?int = auth.SentCode;
-			var _flags = readUInt32LE(body, 0)
-			var request={}
+			_flags = readUInt32LE(body, 0)
 			if(readUInt32LE(body, 4) == 0xab03c6d9 ){
 				request = {flags:"uint4",type:"uint4",pattern:"string",phone_code_hash:"string"}
 			}else{
 				request = {flags:"uint4",type:"uint4",length:"uint4",phone_code_hash:"string"}
 			}
-			if(_flags & 0x2) request.next_type="uint4"
-			if(_flags & 0x4) request.timeout="uint4"
-			ret = pick_out(body,request)
+			if(_flags & (1 << 1)) request.next_type="uint4"
+			if(_flags & (1 << 2)) request.timeout="uint4"
 			break
 		}
 		case 0xcd050916:{// flags:# tmp_sessions:flags.0?int user:User
-			var _flags = readUInt32LE(body, 0)
-			var request={}
-			if(_flags & 0x1){
-				request.tmp_sessions="uint4"
-			}else {
-				if(readUInt32LE(body, 4) == 0x938458c1 ){// flags:# self:flags.10?true contact:flags.11?true mutual_contact:flags.12?true deleted:flags.13?true bot:flags.14?true bot_chat_history:flags.15?true bot_nochats:flags.16?true verified:flags.17?true restricted:flags.18?true min:flags.20?true bot_inline_geo:flags.21?true support:flags.23?true scam:flags.24?true id:int access_hash:flags.0?long first_name:flags.1?string last_name:flags.2?string username:flags.3?string phone:flags.4?string photo:flags.5?UserProfilePhoto status:flags.6?UserStatus bot_info_version:flags.14?int restriction_reason:flags.18?Vector<RestrictionReason> bot_inline_placeholder:flags.19?string lang_code:flags.22?string = User;
-					body = body.slice(8) //remove _flags and tl_constructor
-					var flags = readUInt32LE(body, 0)
-					request.flags="uint4"
-					if(flags & 0x400) request.self="true"
-					if(flags & 0x800) request.contact="true"
-					if(flags & 0x1000) request.mutual_contact="true"
-					if(flags & 0x2000) request.deleted="true"
-					if(flags & 0x4000) request.bot="true"
-					if(flags & 0x8000) request.bot_chat_history="true"
-					if(flags & 0x10000) request.bot_nochats="true"
-					if(flags & 0x20000) request.verified="true"
-					if(flags & 0x40000) request.restricted="true"
-					if(flags & 0x80000) request.min="true"
-					if(flags & 0x100000) request.bot_inline_geo="true"
-					if(flags & 0x200000) request.support="true"
-					if(flags & 0x400000) request.scam="true"
-					request.id="uint4"
-					if(flags & 0x1) request.access_hash="long"
-					if(flags & 0x2) request.first_name="string"
-					if(flags & 0x4) request.last_name="string"
-					if(flags & 0x8) request.username="string"
-					if(flags & 0x10) request.phone="string"
-					if(flags & 0x20) request.photo="userprofilephoto"
-					if(flags & 0x40) request.status="uint4"
-					if(flags & 0x4000) request.bot_info_version="uint4"
-					if(flags & 0x40000) request.restriction_reason="vector" //todo
-					if(flags & 0x80000) request.bot_inline_placeholder="string"
-					if(flags & 0x400000) request.lang_code="string"
-				}
-			}
-			ret = pick_out(body,request)
+			_flags = readUInt32LE(body, 0)
+			request={flags:"uint4"}
+			if(_flags & (1 << 0)) request.tmp_sessions="uint4"
+			request.user="User"
 			break
 		}
 		case 0x2144ca19:{//RPCerror error:int message:string
-			ret = pick_out(body,{error:"uint4",error_text:"string"})
+			request = {error:"uint4",error_text:"string"}
+			break
+		}
+		case 0x938458c1:{// flags:# self:flags.10?true contact:flags.11?true mutual_contact:flags.12?true deleted:flags.13?true bot:flags.14?true bot_chat_history:flags.15?true bot_nochats:flags.16?true verified:flags.17?true restricted:flags.18?true min:flags.20?true bot_inline_geo:flags.21?true support:flags.23?true scam:flags.24?true id:int access_hash:flags.0?long first_name:flags.1?string last_name:flags.2?string username:flags.3?string phone:flags.4?string photo:flags.5?UserProfilePhoto status:flags.6?UserStatus bot_info_version:flags.14?int restriction_reason:flags.18?Vector<RestrictionReason> bot_inline_placeholder:flags.19?string lang_code:flags.22?string = User;
+			_flags = readUInt32LE(body, 0)
+			request={flags:"uint4"}
+			if(_flags & (1 << 10)) request.self="true"
+			if(_flags & (1 << 11)) request.contact="true"
+			if(_flags & (1 << 12)) request.mutual_contact="true"
+			if(_flags & (1 << 13)) request.deleted="true"
+			if(_flags & (1 << 14)) request.bot="true"
+			if(_flags & (1 << 15)) request.bot_chat_history="true"
+			if(_flags & (1 << 16)) request.bot_nochats="true"
+			if(_flags & (1 << 17)) request.verified="true"
+			if(_flags & (1 << 18)) request.restricted="true"
+			if(_flags & (1 << 20)) request.min="true"
+			if(_flags & (1 << 21)) request.bot_inline_geo="true"
+			if(_flags & (1 << 23)) request.support="true"
+			if(_flags & (1 << 24)) request.scam="true"
+			request.id="uint4"
+			if(_flags & (1 << 0)) request.access_hash="long"
+			if(_flags & (1 << 1)) request.first_name="string"
+			if(_flags & (1 << 2)) request.last_name="string"
+			if(_flags & (1 << 3)) request.username="string"
+			if(_flags & (1 << 4)) request.phone="string"
+			if(_flags & (1 << 5)) request.photo="UserProfilePhoto"
+			if(_flags & (1 << 6)) request.status="UserStatus"
+			if(_flags & (1 << 14)) request.bot_info_version="uint4"
+			if(_flags & (1 << 18)) request.restriction_reason="Vector<RestrictionReason>"
+			if(_flags & (1 << 19)) request.bot_inline_placeholder="string"
+			if(_flags & (1 << 22)) request.lang_code="string"
+			break	
+		}
+		case 0xecd75d8c:{// photo_id:long photo_small:FileLocation photo_big:FileLocation dc_id:int = UserProfilePhoto
+			request={ photo_id:"long", photo_small:"FileLocation", photo_big:"FileLocation", dc_id:"uint4"}
+			break
+		}
+		case 0xbc7fc6cd: {// volume_id:long local_id:int = FileLocation;
+			request={ volume_id:"long", local_id:"uint4"}
+			break
+		}
+		case 0x8c703f :{//userStatusOffline#8c703f was_online:int = UserStatus;
+			request={ was_online:"uint4"}
 			break
 		}
 		default:{
+			request = {}
 			console.log('Unknown tl_constructor 0x'+tl_constructor.toString(16)+' add it at tlg.js line  ~155')
 			console.hex(body)
 			break
 		}
 	}
+	if( request == {} ){ //if constructor not have fields
+		len = 0
+		ret = tl_constructor
+	} else {
+		res = pick_out(body,request)
+		len = res.arr.length
+		ret = res.out
+	}
+
+	if(isrec) return {len, ret}
 	return ret
 }
 
@@ -260,22 +284,14 @@ function pick_out(arr,tl_info){
 				out[properties[j]] = true
 				break
 			}
-			case "userprofilephoto":{
-				if(readUInt32LE(arr, 0) == 0xecd75d8c ){
-					out[properties[j]] = readBigIntFromBuffer(arr.slice(0,8), true, true)
-					//skip filelocation
-					arr = arr.slice(48)
-				}else{
-					arr = arr.slice(4)
-				}
-				break
-			}
 			default:{
-				console.log('Unknown type add it at tlg.js line  ~234')
+				res = parse_answer(arr,true)
+				out[properties[j]] = res.ret
+				arr = arr.slice(-res.len)
 			}
 		}
 	}	
-	return out
+	return {arr, out}
 }
 function include(url) {
   var s = document.createElement("script");
